d5b5691008d928f4b42477be4be1dd51b762b006
pcampones <p.campones@campus.fct.unl.pt>
2024-03-10 17:34:19 +0000

    ABD client. I haven't tested with many concurrent clients but so far so good.

--- src/client/ABDClient.java
+++ src/client/ABDClient.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
@@ -20,19 +21,21 @@ import java.util.stream.Collectors;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 
-public class ClientSingleWriter {
 
-    private static final Logger logger = LogManager.getLogger(ClientSingleWriter.class);
+public class ABDClient {
+
+    private static final Logger logger = LogManager.getLogger(ABDClient.class);
 
     private final List<WebTarget> targets;
-    
-    private int timestamp = 0;
 
     private final int readQuorum;
 
     private final int writeQuorum;
 
-    public ClientSingleWriter() throws IOException {
+    private final int writerId;
+
+    public ABDClient(int writerId) throws IOException {
+        this.writerId = writerId;
         Properties props = new Properties();
         props.load(Files.newInputStream(Paths.get("config/config.properties")));
         this.readQuorum = Integer.parseInt(props.getProperty("read_quorum", "5"));
@@ -45,8 +48,56 @@ public class ClientSingleWriter {
         }
     }
 
-    @SneakyThrows
     public float read() {
+        logger.info("Issuing read request");
+        logger.trace("Reading from quorum");
+        List<RegisterContentPojo> quorumResponses = getReadQuorum();
+        logger.trace("Processing read quorum");
+        RegisterContentPojo result = getMostUpdatedContent(quorumResponses);
+        logger.debug("Highest timestamp: {}, lowest writer ID: {}, value: {}",
+                result.getTimestamp(), result.getId(), result.getValue());
+        broadcastWrite(result);
+        return result.getValue();
+    }
+
+    private RegisterContentPojo getMostUpdatedContent(Collection<RegisterContentPojo> quorumResponses) {
+        int highestTimestamp = quorumResponses.stream()
+                .mapToInt(RegisterContentPojo::getTimestamp)
+                .max()
+                .orElseThrow(() -> new IllegalStateException("No timestamps received"));
+        List<RegisterContentPojo> highestTimestampResponses = quorumResponses.stream()
+                .filter(response -> response.getTimestamp() == highestTimestamp)
+                .collect(Collectors.toList());
+        int lowestWriterId = highestTimestampResponses.stream()
+                .mapToInt(RegisterContentPojo::getId)
+                .min()
+                .orElseThrow(() -> new IllegalStateException("No writer IDs received"));
+        return highestTimestampResponses.stream()
+                .filter(response -> response.getId() == lowestWriterId)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("No result found"));
+    }
+
+    public void write(float value) {
+        logger.info("Issuing write request");
+        logger.trace("Reading most updated timestamp");
+        int timestamp = readMostUpdatedTimestamp();
+        logger.trace("Most updated timestamp: {}", timestamp);
+        RegisterContentPojo content = new RegisterContentPojo(timestamp + 1, writerId, value);
+        broadcastWrite(content);
+    }
+
+    @SneakyThrows
+    private int readMostUpdatedTimestamp() {
+        List<RegisterContentPojo> quorumResponses = getReadQuorum();
+        return quorumResponses.stream()
+                .mapToInt(RegisterContentPojo::getTimestamp)
+                .max()
+                .orElseThrow(() -> new IllegalStateException("No timestamps received"));
+    }
+
+    @SneakyThrows
+    private List<RegisterContentPojo> getReadQuorum() {
         logger.trace("Issuing read request");
         BlockingQueue<RegisterContentPojo> responses = new LinkedBlockingQueue<>();
         List<RegisterContentPojo> quorumResponses = new ArrayList<>(readQuorum);
@@ -54,7 +105,7 @@ public class ClientSingleWriter {
             new Thread(() -> readFromReplica(target, responses)).start();
         while (quorumResponses.size() < readQuorum)
             quorumResponses.add(responses.take());
-        return processReadQuorum(quorumResponses);
+        return quorumResponses;
     }
 
     private static void readFromReplica(WebTarget target, BlockingQueue<RegisterContentPojo> responses) {
@@ -64,43 +115,22 @@ public class ClientSingleWriter {
         responses.add(registerContent);
     }
 
-    private static Float processReadQuorum(List<RegisterContentPojo> quorumResponses) {
-        int highestTimestamp = quorumResponses.stream()
-                .mapToInt(RegisterContentPojo::getTimestamp)
-                .max()
-                .orElseThrow(() -> new IllegalStateException("No responses received"));
-        List<RegisterContentPojo> highTimestampResponses = quorumResponses.stream()
-                .filter(r -> r.getTimestamp() == highestTimestamp)
-                .collect(Collectors.toList());
-        int lowestId = highTimestampResponses.stream()
-                .mapToInt(RegisterContentPojo::getId)
-                .min()
-                .orElseThrow(() -> new IllegalStateException("No responses received"));
-        return highTimestampResponses.stream()
-                .filter(r -> r.getId() == lowestId)
-                .map(RegisterContentPojo::getValue)
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("No responses received"));
-    }
-
     @SneakyThrows
-    public void write(float value) {
-        logger.trace("Issuing write request with value: {}", value);
+    private void broadcastWrite(RegisterContentPojo content) {
+        logger.trace("Issuing write request with value: {}", content.getValue());
         BlockingQueue<Boolean> responses = new LinkedBlockingQueue<>();
         int numResponses = 0;
-        RegisterContentPojo registerContent = new RegisterContentPojo(timestamp, 0, value);
         for (WebTarget target : targets)
-            new Thread(() -> writeToReplica(target, registerContent, responses)).start();
+            new Thread(() -> writeToReplica(target, content, responses)).start();
         while (numResponses < writeQuorum) {
             responses.take();
             numResponses++;
         }
-        timestamp++;
-        logger.debug("Incremented timestamp to: {}", timestamp);
     }
 
     private void writeToReplica(WebTarget target, RegisterContentPojo registerContent, BlockingQueue<Boolean> responses) {
         target.path("write").request().post(Entity.entity(registerContent, APPLICATION_JSON_TYPE));
         responses.add(true);
     }
+
 }
--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -10,14 +10,20 @@ public class ClientMain {
     private static final Logger logger = LogManager.getLogger(ClientMain.class);
 
     public static void main(String[] args) throws IOException {
-        ClientSingleWriter client = new ClientSingleWriter();
+        if (args.length < 1) {
+            System.out.println("Usage: java ClientMain <processId>");
+            System.out.println("processId: ID of the client (integer). Each client must have a distinct ID!");
+            System.exit(-1);
+        }
+        int writerId = Integer.parseInt(args[0]);
+        ABDClient client = new ABDClient(writerId);
         for (int i = 0; i < 10; i++) {
             write(i, client);
             read(i, client);
         }
     }
 
-    private static void write(int value, ClientSingleWriter client) {
+    private static void write(int value, ABDClient client) {
         logger.info("Issuing write request with value: {}", value);
         long start = System.currentTimeMillis();
         client.write(value);
@@ -25,7 +31,7 @@ public class ClientMain {
         logger.debug("Write request took: {}ms", end - start);
     }
 
-    private static void read(int i, ClientSingleWriter client) {
+    private static void read(int i, ABDClient client) {
         logger.info("Issuing read request on iteration: {}", i);
         long start = System.currentTimeMillis();
         float value = client.read();
